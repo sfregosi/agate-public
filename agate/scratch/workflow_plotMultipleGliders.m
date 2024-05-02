6a73c0fa6a50569b62ba7b0683ddaa0143efe4b8
sfregosi <22456126+sfregosi@users.noreply.github.com>
2024-05-02 10:24:46 -0700

    plot fkw detections for two gliders 2022

--- agate/scratch/workflow_plotMultipleGliders.m
+++ agate/scratch/workflow_plotMultipleGliders.m
@@ -26,62 +26,84 @@ col_rt = [0 0 0];       % realized track
 col_ce = [1 1 0.2];     % cetacean events - yellow
 
 % generate the basemap with bathymetry as figure 82, don't save .fig file
-[baseFig] = createBasemap(CONFIG, 1, 82); 
+[baseFig] = createBasemap(CONFIG, 1, 82);
 baseFig.Position = [20    80    1200    700];
+
+%% Glider 1 - SG639
 
 % add original targets
 targetsFile = fullfile(CONFIG.path.mission, 'basestationFiles', 'targets');
-[targets, ~] = readTargetsFile(CONFIG, targetsFile); 
+[targets, ~] = readTargetsFile(CONFIG, targetsFile);
 
 h(1) = plotm(targets.lat, targets.lon, 'Marker', 's', 'MarkerSize', 4, ...
 	'MarkerEdgeColor', [0 0 0], 'MarkerFaceColor', col_pt, 'Color', col_pt, ...
-	'DisplayName', 'planned track');
+	'HandleVisibility', 'off');
 % textm(targets.lat, targets.lon, targets.name, 'FontSize', 10)
 
 % plot realized track
 % load surface positions
-load(fullfile(CONFIG.path.mission, 'profiles', 'sg679_MHI_May2023_gpsSurfaceTable.mat'));
+load(fullfile(CONFIG.path.mission, 'profiles', 'sg639_MHI_Apr2022_gpsSurfaceTable.mat'));
 h(2) = plotm(gpsSurfT.startLatitude, gpsSurfT.startLongitude, ...
-	'Color', col_rt, 'LineWidth', 1.5, 'DisplayName', 'realized track');
+	'Color', col_rt, 'LineWidth', 1.5, 'HandleVisibility', 'off');
+
+% acoustic events
+tl = readtable('C:\Users\Selene.Fregosi\Documents\GitHub\glider-MHI\analysis\fkw_classification\sg639_MHI_Apr2022_PcOnlyForTempMapping.csv');
+
+% load previously created locCalcT
+load(fullfile(CONFIG.path.mission, 'profiles', [CONFIG.gmStr '_locCalcT.mat']));
+% find calculated location for midpoint of event
+tl.midTime = tl.start + (tl.stop - tl.start)/2;
+for f = 1:height(tl)
+	[~, mIdx] = min(abs(locCalcT.dateTime - tl.midTime(f)));
+	tl.lat(f) = locCalcT.latitude(mIdx);
+	tl.lon(f) = locCalcT.longitude(mIdx);
+end
 
 % plot acoustic events
-h(3) = scatterm(tlm.lat, tlm.lon, 30, 'Marker', 'o', ...
+h(3) = scatterm(tl.lat, tl.lon, 80, 'Marker', 'o', ...
 	'MarkerEdgeColor', [0 0 0], 'MarkerFaceColor', col_ce, ...
-	'DisplayName', 'cetacean event');
-
-% add legend
-legend(h,  'Location', 'eastoutside', 'FontSize', 14)
-
-% add title
-title('SG679 MHI May 2023', 'Interpreter', 'none')
+	'DisplayName', 'acoustic event (SG639)');
 
 %% (4) Add second glider to plot
 
-col_ce_sg639 = [1 0.4 0];     % cetacean events - orange
+col_ce_g2 = [1 0.4 0];     % cetacean events - orange
 
 % targets
-targetsFile_sg639 = 'D:\sg639_MHI_Apr2023\piloting\basestationFiles\targets';
-[targets_sg639 ,~] = readTargetsFile(CONFIG, targetsFile_sg639); 
+targetsFile_g2 = 'T:\glider_MHI_spring_2022\sg680_MHI_Apr2022\piloting\basestationFiles\targets';
+[targets_g2 ,~] = readTargetsFile(CONFIG, targetsFile_g2);
 
-h(4) = plotm(targets_sg639.lat, targets_sg639.lon, 'Marker', 's', 'MarkerSize', 4, ...
+h(4) = plotm(targets_g2.lat, targets_g2.lon, 'Marker', 's', 'MarkerSize', 4, ...
 	'MarkerEdgeColor', [0 0 0], 'MarkerFaceColor', col_pt, 'Color', col_pt, ...
 	'HandleVisibility', 'off');
 
 % realized track
-loadTmp = load(fullfile('D:\sg639_MHI_Apr2023\piloting\profiles\sg639_MHI_Apr2023_gpsSurfaceTable.mat'));
-gpsSurfT_sg639 = loadTmp.gpsSurfT;
-h(5) = plotm(gpsSurfT_sg639.startLatitude, gpsSurfT_sg639.startLongitude, ...
-	'Color', col_rt, 'LineWidth', 1.5, 'DisplayName', 'realized track');
+gpsSurfT_g2 = load(fullfile('T:\glider_MHI_spring_2022\sg680_MHI_Apr2022\piloting\profiles\sg680_MHI_Apr2022_gpsSurfaceTable.mat'));
+gpsSurfT_g2 = gpsSurfT_g2.gpsSurfT;
+h(5) = plotm(gpsSurfT_g2.startLatitude, gpsSurfT_g2.startLongitude, ...
+	'Color', col_rt, 'LineWidth', 1.5, 'HandleVisibility', 'off');
 
 % plot acoustic events
-h(6) = scatterm(tlm.lat, tlm.lon, 30, 'Marker', 'o', ...
-	'MarkerEdgeColor', [0 0 0], 'MarkerFaceColor', col_ce, ...
-	'DisplayName', 'cetacean event');
+tl_g2 = readtable('C:\Users\Selene.Fregosi\Documents\GitHub\glider-MHI\analysis\fkw_classification\sg680_MHI_Apr2022_PcOnlyForTempMapping.csv');
+
+% load previously created locCalcT
+locCalcT_g2 = load(fullfile('T:\glider_MHI_spring_2022\sg680_MHI_Apr2022\piloting\profiles\sg680_MHI_Apr2022_locCalcT.mat'));
+locCalcT_g2 = locCalcT_g2.locCalcT;
+% find calculated location for midpoint of event
+tl_g2.midTime = tl_g2.start + (tl_g2.stop - tl_g2.start)/2;
+for f = 1:height(tl_g2)
+	[~, mIdx] = min(abs(locCalcT_g2.dateTime - tl_g2.midTime(f)));
+	tl_g2.lat(f) = locCalcT_g2.latitude(mIdx);
+	tl_g2.lon(f) = locCalcT_g2.longitude(mIdx);
+end
+
+h(6) = scatterm(tl_g2.lat, tl_g2.lon, 80, 'Marker', 'o', ...
+	'MarkerEdgeColor', [0 0 0], 'MarkerFaceColor', col_ce_g2, ...
+	'DisplayName', 'acoustic event (SG680)');
 
 
 % add legend
-legend(h,  'Location', 'eastoutside', 'FontSize', 14)
+legend(h([3,6]),  'Location', 'eastoutside', 'FontSize', 14)
 
 % add title
-title('SG679 MHI May 2023', 'Interpreter', 'none')
+title('False killer whale detections - Spring 2022', 'Interpreter', 'none')
 
